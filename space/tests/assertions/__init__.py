4e2f2d05ab42b4161caf3ce1842d7056e8986f0b
Austin Philp <austinphilp@gmail.com>
2018-11-14 21:17:13 -0800

    Add basic sensor testing

--- space/tests/assertions/__init__.py
+++ space/tests/assertions/__init__.py
@@ -1,4 +1,5 @@
 from space.tests.assertions._acceleration import _test_acceleration
 from space.tests.assertions._rotation import _test_rotation
+from space.tests.assertions._detection import _assert_can_detect
 
-__all__ = ['_test_acceleration', '_test_rotation']
+__all__ = ['_test_acceleration', '_test_rotation', '_assert_can_detect']
