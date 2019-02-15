c15cbf0bbc3fa9a122325ef9b9c1d67157e74de7
Austin Philp <austinphilp@gmail.com>
2019-02-14 17:49:54 -0800

    Improve test coverage

--- space/tests/collisions/test_near_misses.py
+++ space/tests/collisions/test_near_misses.py
@@ -5,7 +5,19 @@ from space.constants import directions
 from space.tests.assertions import _test_collision
 
 
-def test_rear_near_miss():
+def test_rear_near_miss_port():
+    _test_collision(
+        direction_1=directions.FORWARD,
+        pos_1=Point(0, -10, 0),
+        throttle_1=1.0,
+        direction_2=directions.FORWARD,
+        pos_2=Point(100, 0, 0),
+        throttle_2=0.5,
+        expected_collision=False
+    )
+
+
+def test_rear_near_miss_sb():
     _test_collision(
         direction_1=directions.FORWARD,
         pos_1=Point(0, 10, 0),
@@ -17,7 +29,43 @@ def test_rear_near_miss():
     )
 
 
-def test_head_on_near_miss():
+def test_rear_near_miss_oh():
+    _test_collision(
+        direction_1=directions.FORWARD,
+        pos_1=Point(0, 0, 10),
+        throttle_1=1.0,
+        direction_2=directions.FORWARD,
+        pos_2=Point(100, 0, 0),
+        throttle_2=0.5,
+        expected_collision=False
+    )
+
+
+def test_rear_near_miss_deck():
+    _test_collision(
+        direction_1=directions.FORWARD,
+        pos_1=Point(0, 0, -10),
+        throttle_1=1.0,
+        direction_2=directions.FORWARD,
+        pos_2=Point(100, 0, 0),
+        throttle_2=0.5,
+        expected_collision=False
+    )
+
+
+def test_head_on_near_miss_port():
+    _test_collision(
+        direction_1=directions.FORWARD,
+        pos_1=Point(-100, -10, 0),
+        throttle_1=1.0,
+        direction_2=directions.AFT,
+        pos_2=Point(100, 0, 0),
+        throttle_2=1.0,
+        expected_collision=False
+    )
+
+
+def test_head_on_near_miss_sb():
     _test_collision(
         direction_1=directions.FORWARD,
         pos_1=Point(-100, 10, 0),
@@ -29,10 +77,10 @@ def test_head_on_near_miss():
     )
 
 
-def test_angular_near_miss():
+def test_head_on_near_miss_oh():
     _test_collision(
         direction_1=directions.FORWARD,
-        pos_1=Point(-100, 10, 0),
+        pos_1=Point(-100, 0, 10),
         throttle_1=1.0,
         direction_2=directions.AFT,
         pos_2=Point(100, 0, 0),
@@ -41,10 +89,22 @@ def test_angular_near_miss():
     )
 
 
-def test_level_tbone_near_miss():
+def test_head_on_near_miss_deck():
     _test_collision(
         direction_1=directions.FORWARD,
-        pos_1=Point(10, 0, 0),
+        pos_1=Point(-100, 0, -10),
+        throttle_1=1.0,
+        direction_2=directions.AFT,
+        pos_2=Point(100, 0, 0),
+        throttle_2=1.0,
+        expected_collision=False
+    )
+
+
+def test_level_tbone_near_miss_port():
+    _test_collision(
+        direction_1=directions.FORWARD,
+        pos_1=Point(0, -10, 0),
         throttle_1=0.0,
         direction_2=directions.STARBOARD,
         pos_2=Point(0, -100, 0),
@@ -53,13 +113,85 @@ def test_level_tbone_near_miss():
     )
 
 
-def test_top_down_tbone_near_miss():
+def test_level_tbone_near_miss_sb():
     _test_collision(
         direction_1=directions.FORWARD,
-        pos_1=Point(0, 0, 0),
+        pos_1=Point(0, 10, 0),
+        throttle_1=0.0,
+        direction_2=directions.STARBOARD,
+        pos_2=Point(0, -100, 0),
+        throttle_2=1.0,
+        expected_collision=False
+    )
+
+
+def test_level_tbone_near_miss_oh():
+    _test_collision(
+        direction_1=directions.FORWARD,
+        pos_1=Point(0, 0, 10),
+        throttle_1=0.0,
+        direction_2=directions.STARBOARD,
+        pos_2=Point(0, -100, 0),
+        throttle_2=1.0,
+        expected_collision=False
+    )
+
+
+def test_level_tbone_near_miss_deck():
+    _test_collision(
+        direction_1=directions.FORWARD,
+        pos_1=Point(0, 0, -10),
+        throttle_1=0.0,
+        direction_2=directions.STARBOARD,
+        pos_2=Point(0, -100, 0),
+        throttle_2=1.0,
+        expected_collision=False
+    )
+
+
+def test_top_down_tbone_near_miss_port():
+    _test_collision(
+        direction_1=directions.FORWARD,
+        pos_1=Point(0, -10, 0),
         throttle_1=0.0,
         direction_2=directions.DECK,
-        pos_2=Point(0, 10, 100),
+        pos_2=Point(0, 0, 100),
+        throttle_2=1.0,
+        expected_collision=False
+    )
+
+
+def test_top_down_tbone_near_miss_sb():
+    _test_collision(
+        direction_1=directions.FORWARD,
+        pos_1=Point(0, 10, 0),
+        throttle_1=0.0,
+        direction_2=directions.DECK,
+        pos_2=Point(0, 0, 100),
+        throttle_2=1.0,
+        expected_collision=False
+    )
+
+
+def test_top_down_tbone_near_miss_fw():
+    _test_collision(
+        direction_1=directions.FORWARD,
+        pos_1=Point(10, 0, 0),
+        throttle_1=0.0,
+        direction_2=directions.DECK,
+        pos_2=Point(0, 0, 100),
+        throttle_2=1.0,
+        expected_collision=False
+    )
+
+
+def test_top_down_tbone_near_miss_aft():
+    _test_collision(
+        direction_1=directions.FORWARD,
+        pos_1=Point(-10, 0, 0),
+        throttle_1=0.0,
+        direction_2=directions.DECK,
+        pos_2=Point(0, 0, 100),
         throttle_2=1.0,
         expected_collision=False
     )
